cea659c33a92b74a5ad16adf6cb3c15b1e99dddf
RustemYeldessov <eldesov1997@gmail.com>
2024-09-05 22:23:13 +0600

    added brain even game

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/RustemYeldessov/python-project-49/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/RustemYeldessov/python-project-49/actions)
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/b06bcd90fe5ddcf6b5f9/maintainability)](https://codeclimate.com/github/RustemYeldessov/python-project-49/maintainability)
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,35 @@
+from random import randint
+
+
+answer = ''
+
+def is_even(number):
+    return number % 2 == 0
+
+def main():
+    print('Welcome to the Brain Games')
+    user_name = input('May I have your name? ')
+    print(f'Hello, {user_name}!')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    correct_answer = 0
+    while correct_answer != 3:
+        number = randint(1, 100)
+        print(f'Question: {number}')
+        answer = input(f'Your answer: ')
+
+        if (answer == 'yes' and is_even(number)) or (answer == 'no' and not is_even(number)):
+            print('Correct!')
+            correct_answer += 1
+        else:
+            correct_answer = 'yes' if is_even(number) else 'no'
+            print(f"'{answer}' is wrong answer ;(. Correct answer is '{correct_answer}'")
+            break
+
+    if correct_answer == 3:
+        print(f'Congratulations, {user_name}!')
+    else:
+        print(f"Let's try again, {user_name}")
+
+
+if __name__ == '__main__':
+    main()
