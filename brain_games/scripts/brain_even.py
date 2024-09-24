2ba0b075ff0cde9d00aa61a97312e580900c23cc
RustemYeldessov <eldesov1997@gmail.com>
2024-09-24 22:05:02 +0600

    steel trying to fix an error

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -12,23 +12,27 @@ def main():
     print(f'Hello, {user_name}!')
     print('Answer "yes" if the number is even, otherwise answer "no".')
     correct_answer = 0
-    while correct_answer != 3:
+
+    while correct_answer < 3:
         number = randint(1, 100)
         print(f'Question: {number}')
-        answer = input(f'Your answer: ')
+        answer = input(f'Your answer: ').strip().lower()
 
-        if (answer == 'yes' and is_even(number)) or (answer == 'no' and not is_even(number)):
+        # if answer not in ['yes', 'no']:
+        #     print("Invalid input. Please answer 'yes' or 'no'.")
+        #     continue
+
+        expected_answer = 'yes' if is_even(number) else 'no'
+
+        if answer == expected_answer:
             print('Correct!')
             correct_answer += 1
         else:
-            correct_answer = 'yes' if is_even(number) else 'no'
-            print(f"'{answer}' is wrong answer ;(. Correct answer is '{correct_answer}'")
-            break
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{expected_answer}'.")
+            print(f"Let's try again, {user_name}!")
+            return
 
-    if correct_answer == 3:
-        print(f'Congratulations, {user_name}!')
-    else:
-        print(f"Let's try again, {user_name}")
+    print(f'Congratulations, {user_name}!')
 
 
 if __name__ == '__main__':
